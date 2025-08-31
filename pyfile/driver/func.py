4f533ade008eee10860f141fe032e5c7560337aa
DW181012 <davidwu3530885@gmail.com>
2025-08-31 16:28:38 +0100

    defective chi=0 20 trials done

--- pyfile/driver/func.py
+++ pyfile/driver/func.py
@@ -82,7 +82,7 @@ class DRIVER:
 
         self.category = 'pizza_sim/'
         self.exe_name = 'cilia_1e-4_pizza'
-        self.date = '20250825_defective_0_random_phase_17'
+        self.date = '20250825_defective_0_random_phase_20'
         self.dir = f"data/{self.category}{self.date}{self.afix}/"
         
 
